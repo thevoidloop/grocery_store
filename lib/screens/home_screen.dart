3c5344e4b6aeec12bfe6c19ff140b0431b9859b3
DanielMarro <marroquin_daniel@outlook.com>
2022-10-16 18:30:51 -0600

    gestos agregados y animacion de paneles

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:grocery_store/block.dart';
 
 const _backgroundColor = Color(0xFFF6F5F2);
 const _cartBarHeight = 100.0;
+const _panelTransition = Duration(milliseconds: 500);
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,6 +14,33 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final block = GroceryStoreBlock();
+
+  void _onVerticalGesture(DragUpdateDetails details) {
+    //print(details.primaryDelta);
+    if (details.primaryDelta! < -2) {
+      print(details.primaryDelta);
+      block.changeToCart();
+    } else if (details.primaryDelta! > 5) {
+      print(details.primaryDelta);
+      block.changeToNormal();
+    }
+  }
+
+  double _getTopForWhitePanel(GroceryState state, Size size) {
+    if (state == GroceryState.normal) {
+      return -_cartBarHeight;
+    } //else if (state == GroceryState.cart) {
+    return -(size.height - kToolbarHeight - _cartBarHeight / 2);
+    //}
+  }
+
+  double _getTopForBlackPanel(GroceryState state, Size size) {
+    if (state == GroceryState.normal) {
+      return (size.height - kToolbarHeight - _cartBarHeight);
+    } //else if (state == GroceryState.cart) {
+    return _cartBarHeight / 2;
+    //}
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,11 +56,13 @@ class _HomeScreenState extends State<HomeScreen> {
               Expanded(
                   child: Stack(
                 children: [
-                  Positioned(
+                  AnimatedPositioned(
                       left: 0,
                       right: 0,
-                      top: -_cartBarHeight,
+                      top: _getTopForWhitePanel(block.groceryState, size),
                       height: size.height - kToolbarHeight,
+                      duration: _panelTransition,
+                      curve: Curves.decelerate,
                       child: Container(
                         decoration: const BoxDecoration(
                             color: Colors.white,
@@ -40,12 +70,16 @@ class _HomeScreenState extends State<HomeScreen> {
                                 bottomLeft: Radius.circular(30),
                                 bottomRight: Radius.circular(30))),
                       )),
-                  Positioned(
+                  AnimatedPositioned(
                       left: 0,
                       right: 0,
-                      top: size.height - kToolbarHeight - _cartBarHeight,
+                      top: _getTopForBlackPanel(block.groceryState, size),
                       height: size.height,
-                      child: Container(color: Colors.black))
+                      duration: _panelTransition,
+                      curve: Curves.decelerate,
+                      child: GestureDetector(
+                          onVerticalDragUpdate: _onVerticalGesture,
+                          child: Container(color: Colors.black)))
                 ],
               ))
             ]));
